92fd71d9da36cc4f150d1673a16d96d7d21edbe9
Jamie Thompson <bishbashboshjt@gmail.com>
2026-08-23 00:06:24 +0200

    upgrade flagged version

--- project.scala
+++ project.scala
@@ -4,7 +4,7 @@
 //> using dep org.apache.pdfbox:pdfbox:3.0.8
 //> using dep "io.github.bishabosha::enhanced-string-interpolator:1.0.2"
 //> using dep io.github.bishabosha::scala-object-notation:0.4.4
-//> using dep io.github.bishabosha::flagged:1.0.0-RC2
+//> using dep io.github.bishabosha::flagged:1.0.0-RC3
 //> using dep ch.epfl.lamp::steps::0.2.1
 //> using exclude resources
 //> using options -Wall -Werror
